3c76d6d42ebff49817cf3fd2fcc271f60cdc0f70
kanna0000 <kanna@vivaldi.net>
2017-07-03 20:57:57 +0900

    add Node class

--- main.rb
+++ main.rb
@@ -1,9 +1,15 @@
 require "./test"
 
-puts "address: balance: importance: harvestedBlocks:  remoteStatus"
+#puts "address: balance: importance: harvestedBlocks:  remoteStatus"
+#
+#info = gets.split(" ")
+#
+#addr = GetAccountData.new(address: "NAMJLWQLCNDVFJZ5Z6GYLS66TUS7YYAQZS767HX5", #info: info)
+#
+#addr.printAccountData
+#
 
-info = gets.split(" ")
-
-addr = GetAccountData.new(address: "NAMJLWQLCNDVFJZ5Z6GYLS66TUS7YYAQZS767HX5", info: info)
-
-addr.printAccountData
+node = Node.new
+node.node_select
+puts node.heart_beat
+puts node.status
--- test.rb
+++ test.rb
@@ -2,18 +2,41 @@
 require "open-uri"
 require "json"
 
+class Node
+
+  def node_select
+    node_list = ["104.238.161.61", "62.75.251.134", "108.61.182.27", "108.61.168.86"]
+    @selected_node = node_list[rand(node_list.length)]
+  end
+
+  def heart_beat
+    json =  OpenURI.open_uri("http://#{@selected_node}:7890/heartbeat")
+
+    r = JSON.load(json)
+    return r
+  end
+
+  def status
+    json =  OpenURI.open_uri("http://#{@selected_node}:7890/status")
+
+    r = JSON.load(json)
+    return r
+  end
+
+end
+
 class GetAccountData
   attr_accessor :address, :info
   def initialize(address:, info:)
-    self.address = address
-    self.info = info
+    @address = address
+    @info = info
   end
 
   def getAccountData(type="account")
     node_list = ["104.238.161.61", "62.75.251.134", "108.61.182.27", "108.61.168.86"]
     super_node = node_list[rand(node_list.length)]
     path = "account/get"
-    json =  OpenURI.open_uri("http://#{super_node}:7890/#{path}?address=#{self.address}")
+    json =  OpenURI.open_uri("http://#{super_node}:7890/#{path}?address=#{@address}")
 
     r = JSON.load(json)[type]
     return r
